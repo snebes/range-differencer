c224214a201812239b30b90a97deba0d3f3fad04
Steve Nebes <snebes@gmail.com>
2019-01-12 17:41:59 -0600

    RangeDifference

---
+++ tests/RangeComparatorLCSTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * (c) Steve Nebes <snebes@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SN\RangeDifferencer;
+
+use PHPUnit\Framework\TestCase;
+
+class RangeComparatorLCSTest extends TestCase
+{
+    public function testDifferencesIterator()
+    {
+        
+    }
+}
