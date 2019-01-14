05ae5b64fec6d9cb49b741f22d9a3ed31ee87564
Steve Nebes <snebes@gmail.com>
2019-01-14 14:58:40 -0600

    tests

---
+++ tests/Tag/ArgumentComparator.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SN\RangeDifferencer\Tag;
+
+use Exception;
+use OutOfBoundsException;
+
+/**
+ * Takes a String and generates tokens/atoms that can be used by LCS. This comparator is used specifically for arguments
+ * inside HTML tags.
+ */
+class ArgumentComparator implements AtomSplitterInterface
+{
+    /** @var AtomInterface[] */
+    private $atoms = [];
+
+    /**
+     * @param string $s
+     * @throws Exception
+     */
+    public function __construct(string $s)
+    {
+        $this->generateAtoms($s);
+    }
+
+    /**
+     * @param  string $s
+     * @return void
+     * @throws Exception
+     */
+    private function generateAtoms(string $s): void
+    {
+        if (count($this->atoms) > 0) {
+            throw new Exception('Atoms can only be split once.');
+        }
+
+        $currentWord = '';
+
+        for ($i = 0; $i < mb_strlen($s); $i++) {
+            $c = mb_substr($s, $i, 1);
+
+            if ($c == '<' || $c == '>') {
+                if (mb_strlen($currentWord) > 0) {
+                    $this->atoms[] = new TextAtom($currentWord);
+                }
+
+                $this->atoms[] = new TextAtom('' . $c);
+                $currentWord   = '';
+            } else if (DelimiterAtom::isValidDelimiter('' . $c)) {
+                // A delimiter.
+                if (mb_strlen($currentWord) > 0) {
+                    $this->atoms[] = new TextAtom($currentWord);
+                    $currentWord   = '';
+                }
+
+                $this->atoms[] = new DelimiterAtom($c);
+            } else {
+                $currentWord .= $c;
+            }
+        }
+
+        if (mb_strlen($currentWord) > 0) {
+            $this->atoms[] = new TextAtom($currentWord);
+        }
+    }
+
+    /**
+     * @param  int $i
+     * @return AtomInterface
+     * @throws OutOfBoundsException
+     */
+    public function getAtom(int $i): AtomInterface
+    {
+        if ($i < 0 || $i >= count($this->atoms)) {
+            throw new OutOfBoundsException('There is no Atom with index ' . $i);
+        }
+
+        return $this->atoms[$i];
+    }
+
+    /**
+     * @return int
+     */
+    public function getRangeCount(): int
+    {
+        return count($this->atoms);
+    }
+
+    /**
+     * @param  int                      $thisIndex
+     * @param  RangeComparatorInterface $other
+     * @param  int                      $otherIndex
+     * @return bool
+     */
+    public function rangesEqual(int $thisIndex, RangeComparatorInterface $other, int $otherIndex): bool
+    {
+        if ($other instanceof ArgumentComparator) {
+            return $other->getAtom($otherIndex)->equalsIdentifier($this->getAtom($thisIndex));
+        }
+
+        return false;
+    }
+
+    /**
+     * @param  int                      $length
+     * @param  int                      $maxLength
+     * @param  RangeComparatorInterface $other
+     * @return bool
+     */
+    public function skipRangeComparison(int $length, int $maxLength, RangeComparatorInterface $other): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param  int      $startAtom
+     * @param  int|null $endAtom
+     * @return string
+     */
+    public function substring(int $startAtom, ?int $endAtom = null): string
+    {
+        if (null === $endAtom) {
+            $endAtom = count($this->atoms);
+        }
+
+        if ($startAtom == $endAtom) {
+            return '';
+        } else {
+            $result = '';
+
+            for ($i = $startAtom; $i < $endAtom; $i++) {
+                $result .= $this->atoms[$i]->getFullText();
+            }
+
+            return $result;
+        }
+    }
+}
---
+++ tests/Tag/TagAtom.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SN\RangeDifferencer\Tag;
+
+use InvalidArgumentException;
+
+/**
+ * An atom that represents a closing or opening tag.
+ */
+class TagAtom implements AtomInterface
+{
+    /** @var string */
+    private $identifier = '';
+
+    /** @var string */
+    private $internalIdentifiers = '';
+
+    /**
+     * @param  string $s
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $s)
+    {
+        if (!$this->isValidAtom($s)) {
+            throw new InvalidArgumentException('The given string is not a valid tag.');
+        }
+
+        // Remove the < and >.
+        $s = mb_substr($s, 1, -1);
+
+        if (false !== ($pos = mb_strpos($s, ' '))) {
+            $this->identifier          = mb_substr($s, 0, $pos);
+            $this->internalIdentifiers = mb_substr($s, $pos + 1);
+        } else {
+            $this->identifier = $s;
+        }
+    }
+
+    /** {@inheritdoc} */
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    /** {@inheritdoc} */
+    public function getInternalIdentifiers(): string
+    {
+        return $this->internalIdentifiers;
+    }
+
+    /** {@inheritdoc} */
+    public function hasInternalIdentifiers(): bool
+    {
+        return !empty($this->internalIdentifiers);
+    }
+
+    /**
+     * @param  string $s
+     * @return bool
+     */
+    public static function isValidTag(string $s): bool
+    {
+        return
+            0 == mb_strrpos($s, '<') &&
+            mb_strpos($s, '>') == mb_strlen($s) - 1 &&
+            mb_strlen($s) >= 3;
+    }
+
+    /** {@inheritdoc} */
+    public function getFullText(): string
+    {
+        $s = sprintf('<%s', $this->identifier);
+
+        if ($this->hasInternalIdentifiers()) {
+            $s .= sprintf(' %s', $this->internalIdentifiers);
+        }
+
+        $s .= '>';
+
+        return $s;
+    }
+
+    /** {@inheritdoc} */
+    public function isValidAtom(string $s): bool
+    {
+        return self::isValidTag($s);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return sprintf('TagAtom: %s', $this->getFullText());
+    }
+
+    /** {@inheritdoc} */
+    public function equalsIdentifier(AtomInterface $other): bool
+    {
+        return 0 == strcmp($other->getIdentifier(), $this->getIdentifier());
+    }
+}
